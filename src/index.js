21f13b2b98a20a72e5f172c19f6e78ac01d46cef
Dan Barrick <barrick.dan80@gmail.com>
2018-11-15 12:36:55 -0800

    update

--- src/index.js
+++ src/index.js
@@ -4,15 +4,35 @@ const { GraphQLServer } = require('graphql-yoga')
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
 }
 `
-
-// 2
-const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`
+// 1
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }]
+  
+  const resolvers = {
+    Query: {
+      info: () => `This is the API of a Hackernews Clone`,
+      // 2
+      feed: () => links,
+    },
+    // 3
+    Link: {
+      id: (root) => root.id,
+      description: (root) => root.description,
+      url: (root) => root.url,
+    }
   }
-}
 
 // 3
 const server = new GraphQLServer({
